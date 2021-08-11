051b763d2e6f7802a21bf35cda69638888197ff2
daveliew <daveliew@gmail.com>
2021-08-11 21:30:30 +0800

    import axios and created handleSubmit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const handleChange = (e) => {
+    const val = e.target.value;
+    console.log(val);
+    setUrl(val);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <h1>TLDRwithFriends</h1>
-      <form>
-        <input type="text" placeholder="Enter URL" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter URL" onChange={handleChange} />
         <input type="submit" value="Submit" />
       </form>
     </>
