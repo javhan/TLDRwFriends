994bef0c5ac1af7808b49206e64c32471bae8eba
daveliew <daveliew@gmail.com>
2021-08-12 21:05:56 +0800

    created summaries model

--- tldrWF/settings.py
+++ tldrWF/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todos.apps.TodosConfig',
+    'users',
+    'summaries',
     'rest_framework',
     'corsheaders',
-    'users'
 ]
 
 MIDDLEWARE = [
