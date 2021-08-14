c8800aa8c57053feea1ee3b9a234552849b07bd8
daveliew <daveliew@gmail.com>
2021-08-14 14:11:11 +0800

    jwt refresh and access token post method success

--- tldrWF/settings.py
+++ tldrWF/settings.py
@@ -162,7 +162,7 @@ django_heroku.settings(locals())
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':
     [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
