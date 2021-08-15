600b1a410b9188fb640953346366d6e5efb97ba4
WenHan <wenhantan94@gmail.com>
2021-08-15 20:18:14 +0800

    Login sorted + blacklist

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,155 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHistory} from 'react-router-dom';
+import axiosInstance from "../axios";
 import Nav from "./Nav";
 
-function SignIn() {
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+function Copyright() {
   return (
-    <Nav>
-      <div>
-        <h1>Sign In</h1>
-      </div>
-    </Nav>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        TLDRwithFriends
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
 
-export default SignIn;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+  const history = useHistory();
+  const initialFormData = Object.freeze({
+    username: '',
+    password:'',
+  })
+
+  const [formData, updateFormData] = useState(initialFormData);
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim()
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axiosInstance
+    .post(`token/`, {
+      username: formData.username,
+      password: formData.password,
+    })
+    .then((res) => {
+      localStorage.setItem('access_token', res.data.access);
+      localStorage.setItem('refresh_token', res.data.refresh);
+      axiosInstance.defaults.headers['Authorization'] = 
+        'JWT ' + localStorage.getItem('access_token');
+      history.push('/')
+      console.log(res.data)
+    })
+  }
+  
+
+  return (
+    <Nav>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSubmit}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </Nav>
+  );
+}
--- users/views.py
+++ users/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from users.models import CustomUser
 from rest_framework import viewsets, permissions, response, status
+from rest_framework.views import APIView
 from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # CustomUser viewset
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
+    permission_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
 
     def create(self, request, format='json'):
@@ -22,7 +22,31 @@ class CustomUserViewSet(viewsets.ModelViewSet):
                 return response.Response(json, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class BlacklistTokenView(APIView):
+    permission_classes = [permissions.AllowAny]
 
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return response.Response(status=status.HTTP_205_RESET_CONTENT)
+            
+        except Exception as e:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
+# class BlacklistTokenUpdateView(APIView):
+#     permission_classes = [permissions.AllowAny]
+#     authentication_classes = ()
+
+#     def post(self, request):
+#         try:
+#             refresh_token = request.data["refresh_token"]
+#             token = RefreshToken(refresh_token)
+#             token.blacklist()
+#             return response.Response(status=status.HTTP_205_RESET_CONTENT)
+#         except Exception as e:
+#             return response.Response(status=status.HTTP_400_BAD_REQUEST)
 
     # def create(self, request, format='json')):
     #     serializer = UserSerializer(data=request.data)
