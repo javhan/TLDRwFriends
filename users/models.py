d81b2ceadea76c829c6d08a90adb0d205251cc0b
daveliew <daveliew@gmail.com>
2021-08-18 16:20:43 +0800

    fixed username and email duplicate bug

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
     bio = models.TextField(blank=True, null=True)
