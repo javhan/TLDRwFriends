756992b4524f1ed7b92b49c2fb29bb0dcb5765e5
daveliew <daveliew@gmail.com>
2021-08-18 21:46:24 +0800

    fixed package.json and views.py bug

--- client/src/components/SimilarNews.jsx
+++ client/src/components/SimilarNews.jsx
@@ -1,40 +1,69 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const gNewsKey = process.env.REACT_APP_GNEWS_API_KEY;
 
+const RAPIDAPI = process.env.REACT_APP_RAPIDAPI;
+
 const SimilarNews = ({ topic }) => {
-    const [data, setData] = useState("");
+    const [data, setData] = useState([]);
+    const [toggle, setToggle] = useState(false);
 
     // const guardianNews = `https://content.guardian.apis.com/search?q=${topic}&api-key=${gNewsKey}`;
 
-    const gNews = `https://gnews.io/api/v4/search?q=${topic}&token=${gNewsKey}&lang=en`;
-    console.log(gNews);
+    // const gNews = `https://gnews.io/api/v4/search?q=${topic}&token=${gNewsKey}&lang=en`;
+    // console.log(gNews);
+
+    // useEffect(() => {
+    //     fetch(gNews)
+    //         .then((res) => {
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             setData(data.articles[0]);
+    //         });
+    // }, [gNews]);
+
+    const options = {
+        method: "GET",
+        url: "https://free-news.p.rapidapi.com/v1/search",
+        params: { q: `${topic}`, lang: "en" },
+        headers: {
+            "x-rapidapi-key": RAPIDAPI,
+            "x-rapidapi-host": "free-news.p.rapidapi.com",
+        },
+    };
 
     useEffect(() => {
-        fetch(gNews)
+        axios
+            .request(options)
             .then((res) => {
-                return res.json();
+                console.log(res.data);
+                setData(res.data.articles);
+                setToggle(!toggle);
             })
-            .then((data) => {
-                setData(data.articles[0]);
-            });
-    }, [gNews]);
+            .catch((err) => console.log(err));
+    }, [toggle]);
+
+    const mappedArticles = data.map((article, index) => {
+        return (
+            <>
+                <a href={article.link} target="_blank" rel="noreferrer">
+                    <p>{article.title}</p>
+                    <img
+                        src={article.media}
+                        alt={article.summary}
+                        style={{ height: "50px" }}
+                    />
+                </a>
+            </>
+        );
+    });
 
     return (
         <>
             <h1>Similar News</h1>
-            {data && (
-                <>
-                    {" "}
-                    <h3>{data.title}</h3>
-                    <img
-                        src={data.image}
-                        alt={data.description}
-                        style={{ height: "100px" }}
-                    />
-                    <p>{data.description}</p>{" "}
-                </>
-            )}
+            {data ? mappedArticles : <p>Sorry we didn't find anything!</p>}
         </>
     );
 };
