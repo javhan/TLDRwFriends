76f590032b1c8d0c7a2ddbf25b8c01556f581782
WenHan <wenhantan94@gmail.com>
2021-08-19 00:05:43 +0800

    Wednesday midnight check

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from users.models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('bio', 'email', 'first_name', 'id', 'last_name', 'username')
+        fields = ('bio', 'email', 'first_name', 'id', 'last_name', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length' : 6, 'required': True}}
 
     def create(self, validated_data):
